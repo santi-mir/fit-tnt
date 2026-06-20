60b19de55d99936a28db4988f1f9e33072f2d815
no_one <thebrokenpipe@proton.me>
2026-06-20 13:48:15 +0100

    refactor: mean squared error uses squared sum under the hood.

--- src/meanSquaredError.ts
+++ src/meanSquaredError.ts
@@ -1,3 +1,4 @@
+import { squaredSum } from './squaredSum';
 import { AnyMatrix } from './types';
 
 /**
@@ -9,21 +10,11 @@ import { AnyMatrix } from './types';
  */
 export function meanSquaredError(X: AnyMatrix, Y: AnyMatrix, B: AnyMatrix) {
   const Err: AnyMatrix = X.mmul(B).sub(Y);
-  const { rows: samples, columns: coeffs } = Err;
+  const samples = Err.rows;
 
-  const result: number[] = new Array(B.columns).fill(0) as number[];
-  if (Err.isColumnVector()) {
-    return [Err.dot(Err) / samples];
-  } else {
-    // square of each number in the matrix, and add it up.
-    for (let i = 0; i < samples; i++) {
-      for (let j = 0; j < coeffs; j++) {
-        result[j] += Err.get(i, j) ** 2;
-      }
-    }
-    for (let v = 0; v < result.length; v++) {
-      result[v] /= samples;
-    }
+  const result = squaredSum(Err, { by: 'column' });
+  for (let v = 0; v < result.length; v++) {
+    result[v] /= samples;
   }
   return result;
 }
